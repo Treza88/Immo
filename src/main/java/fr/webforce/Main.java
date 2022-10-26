7de7fb41788a1592f65a692555f650af27f17e8e
Herve Thomesse <treza88@hotmail.com>
2022-10-26 20:22:14 +0200

    Trois mode de recherche

--- src/main/java/fr/webforce/Main.java
+++ src/main/java/fr/webforce/Main.java
@@ -5,16 +5,119 @@ import fr.webforce.entities.Bien;
 import fr.webforce.services.CommandSQL;
 
 import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        String types[] = {"Maison", "Appartement", "Garage", "Terrain vide", "Jardin"};
+        String numeros[] = {"56", "95", "87", "26", "123", "5624"};
+        String rues[] = {"Rue massena", "Boulvard du tilleul", "Impasse cordont", "Cité des gardins", "Place principale"};
+        String codePostals[] = {"59641", "88456", "51369", "02569", "75000"};
+        String villes[] = {"Tartampion", "Nowhere", "Top ville", "Bad ville", "Groland ville"};
+        int nbPieces[] = {1, 2, 3, 4, 5};
+        int surfHabits[] = {100, 210, 153, 72, 165};
+        int prixs[] = {150000, 220000, 320000, 470000, 510000};
+        int anneeConsts[] = {1954, 2010, 2004, 1987, 1674};
 
-        Bien maison1 = new Bien("maison", 4, 70, 120000, 1920, true, true, true, true);
-        Adresse adresse1 = new Adresse("25", "rue du jardinaet", "59000", "Lille");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Entrer le type de logement : ");String  type = sc.nextLine();
+//        sc.nextLine();
+//        System.out.println("Entrer le nombre de piece : ");int nbPiece = sc.nextInt();
+//        System.out.println("Entrer la surface Habitable : ");int surfHabit = sc.nextInt();
+//        System.out.println("Entrer le prix : ");int prix = sc.nextInt();
+//        System.out.println("Entrer l'année de construction' : ");int anneeConst = sc.nextInt();
+//        System.out.println("Il y a t'il un garage? true/false ");Boolean garage = sc.nextBoolean();
+//        System.out.println("Il y a t'il une terrasse? true/false  : ");Boolean terrasse = sc.nextBoolean();
+//        System.out.println("Il y a t'il un balcon? true/false : ");Boolean balcon = sc.nextBoolean();
+//        System.out.println("Il y a t'il un jardin? true/false : ");Boolean jardin = sc.nextBoolean();
+//        System.out.println("Entrer le numéro du logement : ");String numero = sc.nextLine();
+//        sc.nextLine();
+//        System.out.println("Entrer la rue : ");String rue = sc.nextLine();
+//        sc.nextLine();
+//        System.out.println("Entrer le code postal : ");String codePostal = sc.nextLine();
+//        sc.nextLine();
+//        System.out.println("Entrer la ville : ");String ville = sc.nextLine();
+//
+//        Bien maison1 = new Bien(type, nbPiece, surfHabit, prix, anneeConst, garage, terrasse, balcon, jardin);
+//        Adresse adresse1 = new Adresse(numero, rue, codePostal, ville);
 
-        CommandSQL insert1 = new CommandSQL();
-        insert1.insert(maison1, adresse1);
-        CommandSQL trouveTout = new CommandSQL();
-        trouveTout.findAll();
+        //for (int i = 0; i < 25; i++) {
+
+        Bien maison1 = new Bien(getRandomString(types), getRandomInt(nbPieces), getRandomInt(surfHabits), getRandomInt(prixs), getRandomInt(anneeConsts), true, true, true, true);
+        Adresse adresse1 = new Adresse(getRandomString(numeros), getRandomString(rues), getRandomString(codePostals), getRandomString(villes));
+
+//        CommandSQL insert1 = new CommandSQL();
+//        insert1.insert(maison1, adresse1);
+
+//        CommandSQL trouverParId = new CommandSQL();
+//        trouverParId.findById();
+
+//        CommandSQL modifier = new CommandSQL();
+//        modifier.update(maison1, adresse1);
+
+//        CommandSQL supprimmer = new CommandSQL();
+//        supprimmer.delete(adresse1);
+
+//        CommandSQL trouveTout = new CommandSQL();
+//        trouveTout.findAll();
+
+        //}
+        System.out.println("Que voulez vous faire ? ");
+        System.out.println("\t1 - Une recherche par type");
+        System.out.println("\t2 - Une recherche par ville");
+        System.out.println("\t3 - Une recherche par prix");
+        System.out.println("\t4 - Inserer un bien ");
+        System.out.println("\t5 - Une mise a jour d'un bien");
+        System.out.println("\t6 - Une supperession d'un bien apres une recherche");
+        int choix = sc.nextInt();
+        CommandSQL recherchType1 = new CommandSQL();
+        String table = "";
+        switch (choix){
+            case 1:
+
+                recherchType1.recherchType(1, "bien");
+
+                break;
+            case 2:
+                recherchType1.recherchType(2,"adresse");
+
+                break;
+            case 3:
+                recherchType1.recherchType(3,"bien");
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            default:
+
+        }
+
+
+
+
+
+
+
+
     }
+
+    public static int getRandomInt(int[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+    public static String getRandomString(String[] array) {
+        Random rnd = new Random();
+        int index = rnd.nextInt(array.length);
+        return array[index];
+    }
+
 }
--- src/main/java/fr/webforce/services/CommandSQL.java
+++ src/main/java/fr/webforce/services/CommandSQL.java
@@ -1,12 +1,16 @@
 package fr.webforce.services;
 
 
+import com.mysql.cj.protocol.Resultset;
 import fr.webforce.configurations.Connect;
 
 import fr.webforce.entities.Adresse;
 import fr.webforce.entities.Bien;
 
 import java.sql.*;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
 
 public class CommandSQL {
 
@@ -42,11 +46,51 @@ public class CommandSQL {
 
     }
 
-    public void insert(Bien bien, Adresse adresse) throws SQLException {
-        int aze;
+    public void findById() {
+        Scanner sc = new Scanner(System.in);
+        int id = 1;
+        System.out.println("Entrez l'id recherché : ");
+        id = sc.nextInt();
+
         Connection gc = Connect.getConnection();
-        String[] id_col = {"id_adresse"};
-        try (PreparedStatement st = gc.prepareStatement("INSERT INTO adresse (numero,rue,code_postal,ville) VALUES (?,?,?,?)", id_col)) {
+
+        try (PreparedStatement st = gc.prepareStatement("SELECT b.id_bien,b.type,b.nb_piece, " +
+                "b.surf_habit,b.prix,b.annee_const,b.garage,b.terrasse,b.balcon,b.jardin,a.numero," +
+                "a.rue,a.code_postal,a.ville FROM bien b JOIN adresse a on b.id_adresse=a.id_adresse where id_bien = ?")) {
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(rs.getInt("id_bien") + ", ");
+                sb.append(rs.getString("type") + ", ");
+                sb.append(rs.getInt("nb_piece") + ", ");
+                sb.append(rs.getInt("surf_habit") + ", ");
+                sb.append(rs.getInt("prix") + ", ");
+                sb.append(rs.getInt("annee_const") + ", ");
+                sb.append(rs.getBoolean("garage") + ", ");
+                sb.append(rs.getBoolean("terrasse") + ", ");
+                sb.append(rs.getBoolean("balcon") + ", ");
+                sb.append(rs.getBoolean("jardin") + ", ");
+                //sb.append(rs.getInt("id_adresse") + ", ");
+
+                //sb.append(rs.getInt("id_adresse") + ", ");
+                sb.append(rs.getString("numero") + ", ");
+                sb.append(rs.getString("rue") + ", ");
+                sb.append(rs.getString("code_postal") + ", ");
+                sb.append(rs.getString("ville") + ", ");
+                System.out.println(sb);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void insert(Bien bien, Adresse adresse) throws SQLException {
+        int foreignKey;
+        Connection gc = Connect.getConnection();
+        String[] column = {"id_adresse"};
+        try (PreparedStatement st = gc.prepareStatement("INSERT INTO adresse (numero,rue,code_postal,ville) VALUES (?,?,?,?)", column)) {
             st.setString(1, adresse.getNumero());
             st.setString(2, adresse.getRue());
             st.setString(3, adresse.getCode_postal());
@@ -58,8 +102,8 @@ public class CommandSQL {
             }
             try (ResultSet generatedKeys = st.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
-                    aze = generatedKeys.getInt(1);
-                    System.out.println(aze);
+                    foreignKey = generatedKeys.getInt(1);
+                    System.out.println(foreignKey);
                 } else {
                     throw new SQLException("Pas d'insertion, pas d'id_adresse obtenu.");
                 }
@@ -68,21 +112,185 @@ public class CommandSQL {
             throw new RuntimeException(e);
         }
 
-        Connection gc3 = Connect.getConnection();
-        try (PreparedStatement st2 = gc3.prepareStatement("INSERT INTO bien VALUES(null,?,?,?,?,?,?,?,?,?,?)")) {
-            st2.setString(1, bien.getType());
-            st2.setInt(2, bien.getNb_piece());
-            st2.setInt(3, bien.getSurf_habit());
-            st2.setInt(4, bien.getPrix());
-            st2.setInt(5, bien.getAnnee_const());
-            st2.setBoolean(6, bien.isGarage());
-            st2.setBoolean(7, bien.isTerrase());
-            st2.setBoolean(8, bien.isBalcon());
-            st2.setBoolean(9, bien.isJardin());
-            st2.setInt(10, aze);
-            int nb2 = st2.executeUpdate();
+        try (PreparedStatement st = gc.prepareStatement("INSERT INTO bien VALUES(null,?,?,?,?,?,?,?,?,?,?)")) {
+            st.setString(1, bien.getType());
+            st.setInt(2, bien.getNb_piece());
+            st.setInt(3, bien.getSurf_habit());
+            st.setInt(4, bien.getPrix());
+            st.setInt(5, bien.getAnnee_const());
+            st.setBoolean(6, bien.isGarage());
+            st.setBoolean(7, bien.isTerrase());
+            st.setBoolean(8, bien.isBalcon());
+            st.setBoolean(9, bien.isJardin());
+            st.setInt(10, foreignKey);
+            int nb2 = st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void update(Bien bien, Adresse adresse) {
+        int foreignKey = 1;
+        String[] column = {"id_adresse"};
+        Scanner sc = new Scanner(System.in);
+        int id = 1;
+        System.out.println("Entrez l'id recherché : ");
+        id = sc.nextInt();
+
+
+        Connection gc = Connect.getConnection();
+        try (PreparedStatement st2 = gc.prepareStatement("SELECT id_adresse FROM bien where id_bien=?", column)) {
+
+            st2.setInt(1, id);
+
+            ResultSet rs = st2.executeQuery();
+            if (rs.next()) {
+                //int sb = new StringBuilder();
+                foreignKey = rs.getInt("id_adresse");
+                System.out.println(foreignKey);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        var request = "UPDATE bien SET type = ?, nb_piece = ?, surf_habit = ?, prix = ?, annee_const = ?, garage = ?, terrasse = ?, balcon = ?, jardin = ? WHERE id_bien = ?";
+        try (PreparedStatement st = gc.prepareStatement(request)) {
+
+
+            //ResultSet rs = st.executeQuery();
+            st.setString(1, bien.getType());
+            st.setInt(2, bien.getNb_piece());
+            st.setInt(3, bien.getSurf_habit());
+            st.setInt(4, bien.getPrix());
+            st.setInt(5, bien.getAnnee_const());
+            st.setBoolean(6, bien.isGarage());
+            st.setBoolean(7, bien.isTerrase());
+            st.setBoolean(8, bien.isBalcon());
+            st.setBoolean(9, bien.isJardin());
+            st.setInt(10, id);
+            int nb = st.executeUpdate();
+
+
+            var request2 = "UPDATE adresse SET numero = ?, rue = ?, code_postal = ?, ville = ? WHERE id_adresse = ?";
+            PreparedStatement st2 = gc.prepareStatement(request2);
+            st2.setString(1, adresse.getNumero());
+            st2.setString(2, adresse.getRue());
+            st2.setString(3, adresse.getCode_postal());
+            st2.setString(4, adresse.getVille());
+            st2.setInt(5, foreignKey);
+
+            nb = st.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void delete(Adresse adresse) {
+        Scanner sc = new Scanner(System.in);
+        int id = 1;
+        System.out.println("Entrez l'id à supprimmer : ");
+        id = sc.nextInt();
+        Connection gc = Connect.getConnection();
+        try {
+            PreparedStatement st = gc.prepareStatement("DELETE FROM adresse WHERE id_adresse = ?");
+            st.setInt(1, id);
+            int nb = st.executeUpdate();
+            if (nb > 0) {
+                System.out.println("Le bien à l'id " + id + " a été supprimé");
+            } else {
+                System.out.println("Le bien à l'id " + id + " n'a pas été supprimmé");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void recherchType(int TypeRecherche, String table) {
+        String champRecherche = "";
+        switch (TypeRecherche) {
+            case 1:
+                champRecherche = "type";
+                break;
+            case 2:
+                champRecherche = "ville";
+                break;
+            case 3:
+                champRecherche = "prix";
+                break;
+
+        }
+        Scanner sc = new Scanner(System.in);
+        Connection gc = Connect.getConnection();
+        PreparedStatement st = null;
+        try {
+            if (champRecherche == "ville") {
+                st = gc.prepareStatement("SELECT DISTINCT ville FROM adresse");
+            } else if (champRecherche == "type") {
+                st = gc.prepareStatement("SELECT DISTINCT type FROM bien");
+            } else if (champRecherche == "prix") {
+
+                st = gc.prepareStatement("SELECT DISTINCT prix FROM bien");
+            }
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+
+                String rType = rs.getString(champRecherche);
+                System.out.println(rType);
+            }
+            PreparedStatement st2 = null;
+            if (champRecherche == "ville") {
+                System.out.println("Quel type voulez vous rechercher?");
+                String rtype2 = sc.nextLine();
+                st2 = gc.prepareStatement("SELECT * FROM adresse JOIN bien on adresse.id_adresse=bien.id_adresse WHERE ville=?");
+                st2.setString(1, rtype2);
+            } else if (champRecherche == "type") {
+                System.out.println("Quel type voulez vous rechercher?");
+                String rtype2 = sc.nextLine();
+                st2 = gc.prepareStatement("SELECT * FROM bien JOIN adresse on bien.id_adresse=adresse.id_adresse WHERE type=?");
+                st2.setString(1, rtype2);
+            }
+            else if (champRecherche == "prix") {
+                System.out.println("Donner la fourchette de prix:");
+                System.out.println("Fourchette basse:");
+                int fourchBasse = sc.nextInt();
+                System.out.println("Fourchette haute:");
+                int fourchHaute = sc.nextInt();
+                st2 = gc.prepareStatement("SELECT * FROM bien JOIN adresse on bien.id_adresse=adresse.id_adresse WHERE prix BETWEEN ? AND ?");
+                st2.setInt(1, fourchBasse);
+                st2.setInt(2, fourchHaute);
+            }
+            ResultSet rs2 = st2.executeQuery();
+            while (rs2.next()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(rs2.getInt("id_bien") + ", ");
+                sb.append(rs2.getString("type") + ", ");
+                sb.append(rs2.getInt("nb_piece") + ", ");
+                sb.append(rs2.getInt("surf_habit") + ", ");
+                sb.append(rs2.getInt("prix") + ", ");
+                sb.append(rs2.getInt("annee_const") + ", ");
+                sb.append(rs2.getBoolean("garage") + ", ");
+                sb.append(rs2.getBoolean("terrasse") + ", ");
+                sb.append(rs2.getBoolean("balcon") + ", ");
+                sb.append(rs2.getBoolean("jardin") + ", ");
+                sb.append(rs2.getInt("id_adresse") + ", ");
+
+                sb.append(rs2.getInt("id_adresse") + ", ");
+                sb.append(rs2.getString("numero") + ", ");
+                sb.append(rs2.getString("rue") + ", ");
+                sb.append(rs2.getString("code_postal") + ", ");
+                sb.append(rs2.getString("ville") + ", ");
+                System.out.println(sb);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
